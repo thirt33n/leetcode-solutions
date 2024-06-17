7a1f668efe5df590eb50bd824063bdabe589f09b
Siddharth Pradeep <siddharthpradeep4602@gmail.com>
2024-06-17 11:34:46 +0530

    Time: 14 ms (26.78%) | Memory: 7.1 MB (33.84%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -4,15 +4,7 @@ public:
 
         if(n<=1)
             return n;
-
-        vector<int> fibo(n+1);
-        fibo[0] = 0;
-        fibo[1] = 1;
-                
-        for(int i=2;i<=n;i++)
-        {
-            fibo[i] = fibo[i-1]+fibo[i-2];
-        }
-        return fibo[n];
+       
+        return fib(n-1)+fib(n-2);
     }
 };
