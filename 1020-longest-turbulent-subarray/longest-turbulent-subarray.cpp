bb76934e9af373749230bd32aca107412616502c
Siddharth Pradeep <siddharthpradeep4602@gmail.com>
2024-06-24 12:25:11 +0530

    Time: 60 ms (24.94%) | Memory: 42.7 MB (51.89%) - LeetSync

---
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxTurbulenceSize(vector<int>& arr) {
+        int l = 0,r =1;
+        int counter = 1;
+        string prev = "";
+        
+        while(r<arr.size())
+        {
+            if(arr[r-1]>arr[r] && prev!=">")
+            {
+                counter = max(counter,r-l+1);
+                r++;
+                prev = ">";
+            }
+            else if(arr[r-1]<arr[r] && prev!="<")
+            {
+                counter = max(counter,r-l+1);
+                r++;
+                prev = "<";
+            }
+            else{
+                if(arr[r-1] == arr[r])
+                    r++;
+                l = r-1;
+                prev = "";
+                
+            }
+           
+        }
+         return counter;
+
+    }
+};
