fb4f0e5f74d9b305abf59fc7c3d33b4d068d824c
Siddharth Pradeep <siddharthpradeep4602@gmail.com>
2024-07-09 11:28:11 +0530

    Time: 153 ms (49.71%) | Memory: 76.6 MB (57.65%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
-        // sort(customers.begin(),customers.end(),[](vector<int> a,vector<int> b){return a[0]>b[0];});
         int n= customers.size();
         int curTime = customers[0][0];
-    
-        cout<<curTime<<endl;
+  
         double totWait = 0;
         for(int i=0;i<n;i++)
         {
             if(curTime<customers[i][0])
                 curTime = customers[i][0];
-            
             int wtTime = 0;
             curTime+=customers[i][1];
             wtTime = curTime - customers[i][0];
-
             totWait+=wtTime;
-            cout<<curTime<<" "<<wtTime<<" "<<totWait<<" "<<endl;
         
         }
-        // cout<<totWait;
         return totWait/n;
     }
 };
